91ff0de5b8fd9dc7a409f3421ad12464a72faf43
Shivam Garala <shivamgarala1010@gmail.com>
2026-01-02 22:55:08 +0530

    Update DiamondType and Metal models to allow nullable names; modify controllers to remove name validation checks
    - Changed `type_name` in DiamondType and `metal_name` in Metal models to allow null values.
    - Commented out validation checks for `type_name` and `metal_name` in their respective controllers, simplifying the creation process.
    - Updated controller responses to focus on essential attributes, enhancing API usability.

--- Models/DiamondType.js
+++ Models/DiamondType.js
@@ -5,7 +5,7 @@ const DiamondType = sequelize.define(
     "diamond_types",
     {
         id: { type: Sequelize.BIGINT.UNSIGNED, autoIncrement: true, allowNull: false, primaryKey: true },
-        type_name: { type: Sequelize.STRING(255), allowNull: false },
+        type_name: { type: Sequelize.STRING(255), allowNull: true },
         type_code: { type: Sequelize.STRING(255), allowNull: false },
     },
     {
